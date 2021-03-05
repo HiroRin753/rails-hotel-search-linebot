fa5f2ec4285093f95576c73922a871b45974de6a
HiroRin753 <chikuzen.0306@gmail.com>
2021-03-05 18:12:15 +0900

    lineシークレットイグノア

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -1,8 +1,18 @@
 class LineBotController < ApplicationController
 
   protect_from_forgery except: [:callback]
-  
+
   def callback
 
+  end
+
+  private
+  
+  # Line::Bot::Clientクラスをインスタンス化することで、メッセージの解析や返信などの機能を使うことができるようになる
+  def client
+    @client ||= Line::Bot::Client.new { |config|  #||=は左辺がnilやfalseの場合、右辺を代入するという意味
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
   end
 end
